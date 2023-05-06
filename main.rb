6b306977c3c0f6b328a3daa3b6b6e01835b847b9
Alex <alex.respect555@gmail.com>
2023-05-06 09:39:47 +0300

    shop 2

--- main.rb
+++ main.rb
@@ -10,4 +10,12 @@ products = []
 products << Film.new(title: "Остров", director: "Dicaprio", price: 500, year: 2015, amount: 4)
 products << Book.new(name: "Буратино", author: "Папа Карло", price: 555, year: 2020, amount: 5)
 
+products << Book.new(
+  name: "Италия",
+  author: "Итальянец",
+  price: "300",
+  year: 2010,
+  amount: 10
+)
+
 products.each {|product| puts product}
